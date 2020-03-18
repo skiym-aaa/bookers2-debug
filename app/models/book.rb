3ae1bbb778c19b5d6df325f643e8b140878bd20b
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-18 20:48:14 +0900

    [Finish] 検索機能

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-	#バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
-	#presence trueは空欄の場合を意味する。
+
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/about' => 'home#about'
+  get 'search' => 'searchs#search'
+
   resources :users,only: [:show,:index,:edit,:update] do
     member do
       get :following, :followers
     end
   end
+
   resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
