bcac1f61cff0cf91b47c495c61ea5905fde4077a
skiym-aaa <y.sakiyama3216@gmail.com>
2020-03-21 13:18:11 +0900

    [Add] 住所&郵便番号検索

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,20 @@ class User < ApplicationRecord
     following_relationships.find_by(following_id: other_user.id).destroy
   end
   # ---ここまで---
+
+  # prefecture_codeからprefecture_nameに変換するメソッドを定義
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
+  # ここまで
+
   attachment :profile_image, destroy: false
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
